62b84c8aae9250db36a241786f6006d454394462
José Luis C.D <jlcd0003@red.ujaen.es>
2016-03-13 17:32:58 +0100

    Frequency index finished

--- freq.go
+++ freq.go
@@ -12,7 +12,11 @@ type FrequencyIndex struct {
 	//TokenIds guarda el ID de cada token (en forma de string)
 	TokenIds map[string]int
 
+	//DocIds guarda el ID de cada documento
 	DocIds map[string]int
+
+	TokenNames map[int]string
+	DocNames map[int]string
 	//NextToken guarda el siguiente número de token que se va a utilizar
 	NextToken int
 
@@ -30,6 +34,7 @@ type FrequencyIndex struct {
 func (ind *FrequencyIndex) AddToken(token string) int {
 	if a := ind.TokenIds[token]; a == 0 {
 		ind.TokenIds[token] = ind.NextToken
+		ind.TokenNames[ind.NextToken] = token
 		ind.NextToken++
 		return ind.NextToken-1
 	} else {
@@ -40,6 +45,7 @@ func (ind *FrequencyIndex) AddToken(token string) int {
 func (ind *FrequencyIndex) AddDocument(doc string) int {
 	if a := ind.DocIds[doc]; a == 0 {
 		ind.DocIds[doc] = ind.NextDoc
+		ind.DocNames[ind.NextDoc] = doc
 		ind.NextDoc++
 		return ind.NextDoc-1
 	} else {
@@ -68,7 +74,16 @@ func (ind *FrequencyIndex) AddAndCountToken(doc, token string) {
 }
 
 func NewFrequencyIndex() *FrequencyIndex {
-	return &FrequencyIndex{make(map[string]int), make(map[string]int), 1, 1, make(map[int]map[int]int), sync.Mutex{}}
+	return &FrequencyIndex{
+		make(map[string]int),
+		make(map[string]int),
+		make(map[int]string),
+		make(map[int]string),
+		1,
+		1,
+		make(map[int]map[int]int),
+		sync.Mutex{},
+	}
 }
 
 func (ind* FrequencyIndex) Serialize(file string) {
@@ -80,6 +95,21 @@ func (ind* FrequencyIndex) Serialize(file string) {
 
 	encoder := gob.NewEncoder(stream)
 	encoder.Encode(ind)
+}
+
+func DeserializeFrequencyIndex(file string) *FrequencyIndex {
+	stream, err := os.Open(file)
+	defer stream.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	toRet := new(FrequencyIndex)
+
+	decoder := gob.NewDecoder(stream)
+	decoder.Decode(toRet)
+
+	return toRet
 }
 
 func (tokens StringIterator) IterateFrequencyIndex(document string, index *FrequencyIndex) *FrequencyIndex{
---
+++ goirs-lsindex/main.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+    "gitlab.com/joseluiscd/goirs"
+    "flag"
+    "fmt"
+)
+
+func main() {
+    var file string
+
+    flag.StringVar(&file, "f", "freq.index", "Index file to show")
+    flag.Parse()
+
+    findex := goirs.DeserializeFrequencyIndex(file)
+    if findex == nil {
+        panic("UEUEUEU")
+    }
+
+    var ii int
+    for _ = range findex.TokenIds{
+        ii++
+    }
+    fmt.Println(ii, "tokens únicos")
+
+    var id int
+    for _ = range findex.DocIds{
+        id++
+    }
+    fmt.Println(id, "documentos")
+
+
+}
